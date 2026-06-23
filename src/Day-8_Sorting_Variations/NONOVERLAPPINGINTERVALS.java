7cf393780897f1e4c683f15d07d0b8f06d09d561
apnacollege-demo <sachinyaduvanshi553@gmail.com>
2026-06-23 22:09:42 +0530

    Day 8: Sorting Variations and Interval Problems

---
+++ src/Day-8_Sorting_Variations/NONOVERLAPPINGINTERVALS.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class NONOVERLAPPINGINTERVALS {
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+
+        int count = 0;
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] < end) {
+
+                count++;
+
+            } else {
+
+                end = intervals[i][1];
+            }
+        }
+
+        return count;
+    }
+}
