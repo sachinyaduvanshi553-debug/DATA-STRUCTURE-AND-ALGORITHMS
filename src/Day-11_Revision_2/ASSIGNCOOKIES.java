93df9f0c1ef00e2ac8d9fe050cafe48fc6b095aa
apnacollege-demo <sachinyaduvanshi553@gmail.com>
2026-06-26 22:23:03 +0530

    Day 11: Revision of Binary Search, Sorting and Greedy Patterns

---
+++ src/Day-11_Revision_2/ASSIGNCOOKIES.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class ASSIGNCOOKIES {
+
+    public int findContentChildren(int[] g, int[] s) {
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int child = 0;
+        int cookie = 0;
+
+        while (child < g.length && cookie < s.length) {
+
+            if (s[cookie] >= g[child]) {
+                child++;
+            }
+
+            cookie++;
+        }
+
+        return child;
+    }
+}
